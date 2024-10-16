a66aeaa7138da333f67664c457ebf3781b527b04
jennifer hodge <jennifer.hodge@russdarrow.com>
2024-10-16 07:41:06 -0500

    history text done pon home page

--- src/Pages/RestaurantHomePage.tsx
+++ src/Pages/RestaurantHomePage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./restaurantHomePage.css";
-import mainImage from './images/mainImage.jpeg'
+import mainImage from "./images/mainImage.jpeg";
 
 export default function RestaurantHomePage() {
   return (
@@ -23,7 +23,30 @@ export default function RestaurantHomePage() {
         </div>
       </div>
       <img src={mainImage} alt="salmon" className="main-image" />
-  
+      <div className="anniversary-banner-box">
+        <p>10 YEAR ANNIVERSARY</p>
+      </div>
+
+      <div className="brief-history">
+        <div className="history-header">
+          <h1>MODERN.FRESH.AUTHENTIC</h1>
+        </div>
+        <div className="history-text">
+          <p>
+            Partania's Mediterranean Bistro is a family owned and operated
+            restaurant located in the heart of downtown Chicago on Michigan Avenue. 
+            The restaurant was founded in 2014 by Chef Rick Partania
+            and his wife Jennifer, who wanted to bring the flavors of the Mediterranean
+            to Chicago. The restaurant is named after Chef Partania's
+            grandmother, who was a chef in Greece. The restaurant is known for
+            its fresh, authentic Mediterranean cuisine, and its warm and
+            welcoming atmosphere. The restaurant is open for lunch and dinner,
+            and offers a variety of Mediterranean dishes, including hummus,
+            falafel, and tabbouleh. The restaurant is also known for its wine
+            list, which includes a variety of wines from around the world. 
+          </p>
+        </div>
+      </div>
     </>
   );
 }
