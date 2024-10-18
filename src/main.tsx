69c65ecd518f7f39be4cfaf85c2ef3839556ddd1
jennifer hodge <jennifer.hodge@russdarrow.com>
2024-10-18 18:01:56 -0500

    fix code error

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,16 @@
-import HomePage from './Pages/RestaurantHomePage';
-import ReservationPage from './Pages/ReservationPage';
-import EList from './Pages/EList';
-import ContactUs from './Pages/ContactUs';
-import Menus from './Pages/Menus';
-import  Interior  from './Pages/Interior';
-import PrivateEvents from './Pages/PrivateEvents';
+import HomePage from "./Pages/RestaurantHomePage";
+import ReservationPage from "./Pages/ReservationPage";
+import EList from "./Pages/EList";
+import ContactUs from "./Pages/ContactUs";
+import Menus from "./Pages/Menus";
+import Interior from "./Pages/Interior";
+import PrivateEvents from "./Pages/PrivateEvents";
 
+import ReactDOM from "react-dom/client";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-
-
-
-
-import  ReactDOM  from 'react-dom/client'
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import './index.css'
+import "./index.css";
 
 const router = createBrowserRouter([
   {
@@ -32,32 +27,35 @@ const router = createBrowserRouter([
   },
   {
     path: "/contact-us",
-    element: <ContactUs setUserData={function (_data: { name: string; email: string; message: string; }): void {
-      // throw new Error('Function not implemented.');
-    } } userData={undefined} />,
+    element: (
+      <ContactUs
+        setUserData={function (_data: {
+          name: string;
+          email: string;
+          message: string;
+        }): void {
+          // throw new Error('Function not implemented.');
+        }}
+        userData={undefined}
+      />
+    ),
   },
   {
     path: "/menus",
-    element: <Menus/>,
+    element: <Menus />,
   },
   {
     path: "/interior",
-    element: <Interior/>,
+    element: <Interior />,
   },
   {
     path: "/private-events",
-    element: <PrivateEvents/>,
+    element: <PrivateEvents />,
   },
-
-
-  
-
- 
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
