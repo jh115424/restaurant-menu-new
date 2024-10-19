70823658645525abd475def6697010ee2167ca89
jennifer hodge <jennifer.hodge@russdarrow.com>
2024-10-18 19:39:19 -0500

    one more piece left for home page

--- src/Pages/RestaurantHomePage.tsx
+++ src/Pages/RestaurantHomePage.tsx
@@ -162,7 +162,7 @@ export default function RestaurantHomePage() {
       <div className="footer">
         <p>2024 Partania's Mediterranean Bistro</p>
         <p>634 Michigan Ave, Chicago, IL 60601</p>
-        <p>Phone: 1-312-664-8737</p>
+        <p>Phone: 1-312-664-3746</p>
         <p>Email:  info@partaniasmediterraneanbistro.com</p>
         <p>We look forward to hosting you on one of our private events!</p>
       </div>
